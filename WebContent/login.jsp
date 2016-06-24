f4f95e02580689ca324802aac8d466564d0f91f7
dta98 <dta98@DESKTOP-M0S8RVR>
2016-06-24 12:10:02 +0900

    Dao 작성 부탁드립니다

--- WebContent/login.jsp
+++ WebContent/login.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=EUC-KR"
-	pageEncoding="EUC-KR"%>
+    pageEncoding="EUC-KR"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -7,14 +7,11 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<%
-	String id = (String)session.getAttribute("user_id");
-		if ( id == null || id.equals("")) {
-			response.sendRedirect("login.jsp");
-		} else {
-			response.sendRedirect("loing.servlet");
-		}
-	%>
 
+<form action="loing.servlet" method="post">
+<input type="text" id="id" name="id">
+<input type="password" id="password" name="password">
+<input type="submit">
+</form>
 </body>
 </html>
--- src/Control/Servlet.java
+++ src/Control/Servlet.java
@@ -7,30 +7,34 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-public class MemberServlet extends HttpServlet {
+public class Servlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		processRequest(req, resp);
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		processRequest(req, resp);
 	}
 
-	private void processRequest(HttpServletRequest req,
-			HttpServletResponse resp) {
+	private void processRequest(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		String RequestURL = req.getRequestURI();
 		String contextPath = req.getContextPath();
 		String commend = RequestURL.substring(contextPath.length());
-		
-		
 
+		System.out.println("RequestURL : " + RequestURL);
+		System.out.println("contextPath : " + contextPath);
+		System.out.println("commend : " + commend);
+		
+		if(commend == "/loing.servlet") {
+			
+		}
+		
 	}
 }
