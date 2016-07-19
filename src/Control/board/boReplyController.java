6764afd65a139bfb5cad669015e85e87d8267a25
ITFE-7 <ITFE-7@ITFE-07>
2016-07-19 11:29:26 +0900

    common 폴더 이클립스 단축키 참조
    Signed-off-by: ITFE-7 <ITFE-7@ITFE-07>

--- src/Control/board/boReplyController.java
+++ src/Control/board/boReplyController.java
@@ -2,13 +2,10 @@ package Control.board;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import mypkg.controller.BoardListController;
-import mypkg.model.BoardDao;
 import Control.ControllerForward;
 import Control.SuperController;
 
@@ -17,6 +14,7 @@ public class boReplyController implements SuperController {
 	@Override
 	public ControllerForward doProcess(HttpServletRequest req,
 			HttpServletResponse resp) throws ServletException, IOException {
+				return null;
 
 	}
 
--- src/Control/board/boReplyFormController.java
+++ src/Control/board/boReplyFormController.java
@@ -7,10 +7,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import mypkg.controller.BoardListController;
-import mypkg.model.BoardDao;
 import Control.ControllerForward;
 import Control.SuperController;
+import Model.BoardDao;
 
 public class boReplyFormController implements SuperController {
 
@@ -19,7 +18,7 @@ public class boReplyFormController implements SuperController {
 			HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//depth >= 3, count(*) grupno
-		Model.BoardDao bdao = new Model.BoardDao();
+		BoardDao bdao = new BoardDao();
 		int cnt = bdao.SelectReplyCount(Integer.parseInt(req.getParameter("no")));
 		int depth = Integer.parseInt(req.getParameter("depth"));
 		if( cnt >=10 || depth >= 3){
@@ -29,6 +28,7 @@ public class boReplyFormController implements SuperController {
 			RequestDispatcher dispatcher = req.getRequestDispatcher(url);
 			dispatcher.forward(req, resp);
 		}
+		return null;
 	}
 
 }
