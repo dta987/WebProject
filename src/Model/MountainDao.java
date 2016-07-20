ee46ebfa4df642f96b5f7adffcc2dfd612fe02c1
ITFE-7 <ITFE-7@ITFE-07>
2016-07-20 12:25:29 +0900

    mountaindao 작성완료 mountain 빈에 updatedate 변수 추가

--- src/Model/MountainDao.java
+++ src/Model/MountainDao.java
@@ -6,8 +6,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.sun.org.apache.xpath.internal.operations.Bool;
-
 public class MountainDao extends SuperDao {
 
 	public MountainDao() {
@@ -55,18 +53,21 @@ public class MountainDao extends SuperDao {
 
 		PreparedStatement pstmt = null;
 		int cnt = MyInterface.ERROR_DEFALT;
-		String sql = "update mountains set(MOUNTAIN_NAME, MOUNTAIN_AREA, MOUNTAIN_ADDRESS, MOUNTAIN_IMG, MOUNTAIN_INTRODUCE, MOUNTAIN_THEMA, updatedate =to_date(sysdate, 'yyyy/MM/dd HH:mm:ss') where board_no=?";
+		String sql = "update mountains set(mountain_name=?, mountain_area=?, mountain_address=?, mountain_img=?, mountain_introduce=?, "
+		        + "mountain_thema=?, updatedate =to_date(sysdate, 'yyyy/MM/dd HH:mm:ss') where mountain_no=?";
 		try {
 			if (conn == null) {
 				super.conn = super.getConnection();
 			}
 			pstmt = super.conn.prepareStatement(sql);
 
-			pstmt.setString(1, board.getBoard_title());
-			pstmt.setString(2, board.getBoard_content());
-			pstmt.setString(3, board.getBoard_img());
-			pstmt.setString(4, board.getUser_nickname());
-			pstmt.setInt(5, board.getBoard_no());
+			pstmt.setString(1, mountain.getMountain_name());
+			pstmt.setString(2, mountain.getMountain_address1());
+			pstmt.setString(3, mountain.getMountain_address2());
+			pstmt.setString(4, mountain.getMountain_img());
+			pstmt.setString(5, mountain.getMountain_introduce());
+			pstmt.setInt(6, mountain.getMountain_thema());
+			pstmt.setInt(7, mountain.getMountain_no());
 
 			cnt = pstmt.executeUpdate();
 
@@ -95,24 +96,24 @@ public class MountainDao extends SuperDao {
 	}
 
 	// 게시글 작성
-	public int InsertBoard(Board board) {
+	public int InsertMountain(Mountain mountain) {
 
 		PreparedStatement pstmt = null;
 		int cnt = MyInterface.ERROR_DEFALT;
 
-		String sql = "insert into boards(board_no, board_writ_date, board_update, board_category, board_writer, user_nickname, board_title, board_content, board_img, group_no)"
-				+ " values(board_no_seq.nextval, ?, ?, ?, ?, ?, ?, board_no_seq.currval)";
+		String sql = "insert into mountains(mountain_no, mountain_name, mountain_area, mountain_address, mountain_img, mountain_introduce, mountain_thema, updatedate)"
+		        + " values(mountain_no_seq.nextval, ?, ?, ?, ?, ?, ? , to_date(sysdate, 'yyyy/MM/dd HH:mm:ss')   )";
 		try {
 			if (conn == null) {
 				super.conn = super.getConnection();
 			}
 			pstmt = super.conn.prepareStatement(sql);
-			pstmt.setInt(1, board.getBoard_category());
-			pstmt.setString(2, board.getBoard_writer());
-			pstmt.setString(3, board.getUser_nickname());
-			pstmt.setString(4, board.getBoard_title());
-			pstmt.setString(5, board.getBoard_content());
-			pstmt.setString(6, board.getBoard_img());
+			pstmt.setString(1, mountain.getMountain_name());
+			pstmt.setString(2, mountain.getMountain_address1());
+			pstmt.setString(3, mountain.getMountain_address2());
+			pstmt.setString(4, mountain.getMountain_img());
+			pstmt.setString(5, mountain.getMountain_introduce());
+			pstmt.setInt(6, mountain.getMountain_thema());
 
 			cnt = pstmt.executeUpdate();
 
@@ -140,19 +141,18 @@ public class MountainDao extends SuperDao {
 
 	}
 
-	public List<Board> SelectDataList(int beginRow, int endRow) {
+	public List<Mountain> SelectDataList(int beginRow, int endRow) {
 
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		String sql = "select board_no, board_title, board_writ_date, board_readhit, user_nickname, board_writer, ranking"
-				+ " from"
-				+ " ("
-				+ " select board_no, board_title, board_writ_date, board_readhit, user_nickname, board_writer, rank() over( order by board_no desc ) as ranking"
-				+ " from boards where order_no = 0"
-				+ " )"
-				+ " where ranking between ? and ? ";
+		String sql = "select *"
+		        + " from"
+		        + " ("
+		        + " select mountain_no, mountain_name, mountain_area, mountain_address, mountain_img, "
+		        + "mountain_introduce, mountain_thema, updatedate, rank() over( order by mountain_no desc ) as ranking from mountains  )"
+		        + " where ranking between ? and ? ";
 
-		List<Board> board_lists = new ArrayList<Board>();
+		List<Mountain> mountain_lists = new ArrayList<Mountain>();
 
 		try {
 			if (conn == null) {
@@ -165,15 +165,16 @@ public class MountainDao extends SuperDao {
 			rs = pstmt.executeQuery();
 
 			while (rs.next()) {
-				Board board = new Board();
-				board.setBoard_no(rs.getInt("board_no"));
-				board.setBoard_title(rs.getString("board_title"));
-				board.setBoard_writ_date(rs.getString("board_writ_date"));
-				board.setBoard_update(rs.getString("board_update"));
-				board.setBoard_readhit(rs.getString("board_readhit"));
-				board.setUser_nickname(rs.getString("user_nickname"));
-				board.setBoard_writer("board_writer");
-				board_lists.add(board);
+				Mountain mountain = new Mountain();
+				mountain.setMountain_no(rs.getInt("mountain_no"));
+				mountain.setMountain_address1(rs.getString("mountain_address1"));
+				mountain.setMountain_address2(rs.getString("mountain_address2"));
+				mountain.setMountain_img(rs.getString("mountain_img"));
+				mountain.setMountain_introduce(rs.getString("mountain_introduce"));
+				mountain.setMountain_name(rs.getString("mountain_name"));
+				mountain.setMountain_thema(rs.getInt("mountain_thema"));
+				mountain.setUpdatedate(rs.getString("updatedate"));
+				mountain_lists.add(mountain);
 			}
 
 		} catch (Exception e) {
@@ -191,17 +192,17 @@ public class MountainDao extends SuperDao {
 				e2.printStackTrace();
 			}
 		}
-		return board_lists;
+		return mountain_lists;
 	}
 
-	public List<Board> SelectBoard(int pk) {
+	public List<Mountain> Selectmountain(int pk) {
 
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		String sql = "select board_no, board_title, board_writer, board_writ_date, board_update, board_readhit, user_nickname, board_content, board_img, order_no"
-				+ " from boards where board_no=? order by orderno asc";
+		String sql = "select mountain_no, mountain_name, mountain_area, mountain_address, mountain_img, mountain_introduce,"
+		        + " mountain_thema, updatedate from mountains from mountains where mountain_no=? order by orderno asc";
 
-		List<Board> board_lists = new ArrayList<Board>();
+		List<Mountain> mountain_lists = new ArrayList<Mountain>();
 
 		try {
 			if (conn == null) {
@@ -213,18 +214,17 @@ public class MountainDao extends SuperDao {
 			rs = pstmt.executeQuery();
 
 			while (rs.next()) {
-				Board board = new Board();
-				board.setBoard_no(rs.getInt("board_no"));
-				board.setBoard_title(rs.getString("board_title"));
-				board.setBoard_writer(rs.getString("board_writer"));
-				board.setBoard_writ_date(rs.getString("board_writ_date"));
-				board.setBoard_update(rs.getString("board_update"));
-				board.setBoard_readhit(rs.getString("board_readhit"));
-				board.setUser_nickname(rs.getString("user_nickname"));
-				board.setBoard_content(rs.getString("board_content"));
-				board.setBoard_img(rs.getString("board_img"));
-				board.setOrder_no(rs.getInt("order_no"));
-				board_lists.add(board);
+				Mountain mountain = new Mountain();
+				mountain.setMountain_no(rs.getInt("mountain_no"));
+				mountain.setMountain_address1(rs.getString("mountain_address1"));
+				mountain.setMountain_address2(rs.getString("mountain_address2"));
+				mountain.setMountain_img(rs.getString("mountain_img"));
+				mountain.setMountain_introduce(rs.getString("mountain_introduce"));
+				mountain.setMountain_name(rs.getString("mountain_name"));
+				mountain.setMountain_thema(rs.getInt("mountain_thema"));
+				mountain.setUpdatedate(rs.getString("updatedate"));
+				mountain_lists.add(mountain);
+
 			}
 
 		} catch (Exception e) {
@@ -242,17 +242,15 @@ public class MountainDao extends SuperDao {
 				e2.printStackTrace();
 			}
 		}
-		return board_lists;
+		return mountain_lists;
 	}
 
 	public int selectCount() {
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		String sql = "select count(*) as cnt from boards";
+		String sql = "select count(*) as cnt from mountains";
 
 		int cnt = 0;
-
-		List<Board> board_lists = new ArrayList<Board>();
 
 		try {
 			if (conn == null) {
@@ -283,69 +281,5 @@ public class MountainDao extends SuperDao {
 		}
 		return cnt;
 	}
-
-	//조회수 증가
-	public void updatereadhit(int no) { 
-		PreparedStatement pstmt = null;
-		int cnt = 0;
-		String sql = "update boards set board_readhit=board_readhit+1 whwere board_no=?";
-
-		try {
-			if (conn == null) {
-				super.conn = super.getConnection();
-			}
-			pstmt = super.conn.prepareStatement(sql);
-			pstmt.setInt(1, no);
-
-			cnt = pstmt.executeUpdate();
-
-		} catch (Exception e) {
-			e.printStackTrace();
-
-		} finally {
-			try {
-				if (pstmt != null) {
-					pstmt.close();
-				}
-			} catch (Exception e2) {
-				e2.printStackTrace();
-			}
-		}
-		return;
-
-	}
-/**
- * 
- * @param groupno 게시글의 currval 값
- * @return int sql문 결과 리턴값
- *
- * 한 게시글에 대한 리플 제한 수를 두기 위한 메소드. boReplyController.java 참조
- */
-	public int SelectReplyCount(int groupno) {
-		PreparedStatement pstmt = null ;
-		ResultSet rs = null ;
-		String sql = "select count(*) as cnt from boards where groupno = ?" ;
-		
-		int cnt = 0 ; //없는 경우의 기본 값
-		try {
-			if( this.conn == null ){this.conn = this.getConnection() ;}
-			pstmt = this.conn.prepareStatement(sql) ;
-			pstmt.setInt(1, groupno);
-			rs = pstmt.executeQuery() ;
-			
-			if (rs.next()){
-				cnt = rs.getInt("cnt") ;
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}finally{
-			try{
-				if(rs != null){rs.close() ;}
-				if(pstmt != null){pstmt.close();}
-			}catch (Exception e2) {
-				e2.printStackTrace() ;
-			}
-		}
-		return cnt;
-    }
+	
 }
